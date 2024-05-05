5f78ff60b5f5db31ec715453d0078109c912528d
Victoria Aleksandrova <vicaleksan@gmail.com>
2024-05-05 19:03:41 +0200

    Add alt to images

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 function addCard(card, deleteCallback) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image');
     const cardDescription = cardElement.querySelector('.card__description');
     const likeButton = cardDescription.querySelector('.card__like-button');
     const cardList = document.querySelector('.places__list');
     const deleteButton = cardElement.querySelector('.card__delete-button');
 
-    cardElement.querySelector('.card__image').setAttribute('src', card.link);
+    cardImage.setAttribute('src', card.link);
+    cardImage.setAttribute('alt', card.name);
     cardDescription.querySelector('.card__title').textContent = card.name;
 
     deleteButton.addEventListener('click', deleteCallback);
