1d06b70d7b89597cdb50de4cfaffae48c29bdc8a
Victoria Aleksandrova <vicaleksan@gmail.com>
2024-05-05 19:22:22 +0200

    Extract append

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,24 @@
-function addCard(card, deleteCallback) {
+function createCard(card, deleteCallback) {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
     const cardDescription = cardElement.querySelector('.card__description');
     const likeButton = cardDescription.querySelector('.card__like-button');
-    const cardList = document.querySelector('.places__list');
     const deleteButton = cardElement.querySelector('.card__delete-button');
-
     cardImage.setAttribute('src', card.link);
     cardImage.setAttribute('alt', card.name);
     cardDescription.querySelector('.card__title').textContent = card.name;
 
     deleteButton.addEventListener('click', deleteCallback);
 
-    cardList.append(cardElement);
+    return cardElement;
 };
 
+const cardList = document.querySelector('.places__list');
+
 initialCards.forEach(function(card) {
-    addCard(card, deleteCard);
+    const cardElement = createCard(card, deleteCard);
+    cardList.append(cardElement);
 });
 
 function deleteCard(event) {
